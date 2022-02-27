7acd58a775e3c9022db312365a07dd2fe880c18a
Ahmed M. Raslan <ahm3draslan@gmail.com>
2022-02-27 15:30:13 +0200

    Added home, success and failed views

--- resources/views/payment/failed.blade.php
+++ resources/views/payment/failed.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Test | Checkout</title>
+        <title>Test | Success</title>
         <style>
             body {
                 font-family: 'Nunito';
@@ -11,10 +11,7 @@
         </style>
     </head>
     <body>
-        <form action="{{ route('payment.checkout') }}" method="GET">
-            @csrf
-            <input type="text" name="amount" value="10"><br><br>
-            <input type="submit" value="Pay Now with PayPal">
-        </form>
+        <a>Order failed, due to payment cancelation !</a><br><br>
+        <a>Reference order id : {{ $orderID }}</a><br><br>
     </body>
 </html>
